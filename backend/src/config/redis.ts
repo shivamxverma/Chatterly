111749a9bbb548db2607e1843de5beb9e32369fd
Shivam Verma <shivam.verma.shiva.99@gmail.com>
2025-08-07 11:41:57 +0530

    Fixed Redis error

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -2,17 +2,5 @@ import { Redis } from "ioredis";
 import dotenv from "dotenv";
 dotenv.config();
 let redis: Redis;
-if (process.env.NODE_ENV === "production") {
-  if (!process.env.REDIS_URL) {
-    throw new Error("REDIS_URL environment variable is not defined");
-  }
-  redis = new Redis(process.env.REDIS_URL as string);
-} else {
-  redis = new Redis({
-    host: "localhost",
-    port: 6379,
-  });
-}
-
-
+redis = new Redis(process.env.REDIS_URL as string);
 export default redis;
